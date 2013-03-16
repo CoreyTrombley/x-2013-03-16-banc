fa423f82c1f61ead927f471141a462678280d081
Corey Trombley <corey.trombley33@gmail.com>
2013-03-16 11:53:04 -0400

    completed

---
+++ app/assets/javascripts/banc.js
@@ -0,0 +1,3 @@
+function clear_form() {
+  $('#form').empty();
+}
--- spec/features/banks_spec.rb
+++ spec/features/banks_spec.rb
@@ -15,4 +15,23 @@ describe 'Accounts' do
       page.should have_button('Create Bank')
     end
   end
+
+  describe 'JS cancel_form()' do
+    it 'removes the Bank form', :js => true do
+      visit banks_path
+      click_link('New')
+      click_link('Cancel')
+      page.should_not have_button('Create Bank')
+    end
+  end
+
+  describe 'POST /banks' do
+    it 'Create a bank', :js => true do
+      visit banks_path
+      click_link('New')
+      fill_in('Name', :with => 'BoA')
+      click_button('Create Bank')
+      page.should have_css('#banks li', :count => 1)
+    end
+  end
 end
