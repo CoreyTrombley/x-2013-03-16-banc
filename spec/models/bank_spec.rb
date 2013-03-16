1b21f2aa472b364a89273de35cfd541708bf9ed5
Corey Trombley <corey.trombley33@gmail.com>
2013-03-16 10:21:27 -0400

    created a Bank model test

--- spec/models/bank_spec.rb
+++ spec/models/bank_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+describe Bank do
+
+  describe '.create' do
+    it 'saves a bank to the database' do
+      Bank.create(name: 'BoA')
+      bank = Bank.first
+      expect(bank.name).to eq 'BoA'
+    end
+  end
+
+end
